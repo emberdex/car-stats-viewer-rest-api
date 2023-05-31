7861f2133adae5a91a1936c661bf9cb8d202f687
Toby Jones <github@monotron.me>
2023-05-31 12:36:53 +0100

    - Separate duties of ingress and query operations into separate controllers - Prepare for stable release

--- src/main/java/st/emberdex/csvapi/controller/DataQueryController.java
+++ src/main/java/st/emberdex/csvapi/controller/DataQueryController.java
@@ -5,33 +5,41 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import st.emberdex.csvapi.model.document.VehicleDataPointDocument;
-import st.emberdex.csvapi.model.request.VehicleDataPointRequest;
 import st.emberdex.csvapi.service.VehicleDataPointService;
 
 import java.security.Principal;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RestController
-@RequestMapping("/")
+@RequestMapping("/query")
 @RequiredArgsConstructor
-public class DataIngressController {
+public class DataQueryController {
 
   private final VehicleDataPointService vehicleDataPointService;
 
-  @PostMapping
-  public ResponseEntity<Void> logVehicleDataPoint(
-      @RequestBody VehicleDataPointRequest dataPoint,
-      Principal principal
-  ) {
+  @GetMapping("/all")
+  public ResponseEntity<Map<String, Object>> getDataPoints(Principal principal) {
 
-    vehicleDataPointService.saveNewDataPoint(dataPoint, principal.getName());
+    List<VehicleDataPointDocument> results = vehicleDataPointService.getDataPointsForTenant(principal.getName());
 
-    return ResponseEntity.ok().build();
+    Map<String, Object> responseBody = Map.of(
+        "count", results.size(),
+        "results", results
+    );
+
+    return ResponseEntity.ok(responseBody);
+  }
+
+  @DeleteMapping("/all")
+  public ResponseEntity<Void> purgeDataPointsForTenant(Principal principal) {
+
+    vehicleDataPointService.purgeTenantDataPoints(principal.getName());
+
+    return ResponseEntity.ok(null);
   }
 }
